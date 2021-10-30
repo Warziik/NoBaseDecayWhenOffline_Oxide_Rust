13a40726bd6e8f17bed73e24db10e98a8b9ae483
Alex Chesnay <me@alex-chesnay.fr>
2021-10-30 10:27:25 +0200

    initial commit!

---
+++ NoBaseDecayWhenOffline.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Oxide.Core.Libraries.Covalence;
+using Oxide.Plugins;
+
+namespace Oxide.Plugins
+{
+    [Info("NoBaseDecayWhenOffline", "Warziik", "0.1.0")]
+    [Description("Disable base decaying when all players allowed on tool cupboard are offline.")]
+    class NoBaseDecayWhenOffline: CovalencePlugin
+    {
+        private void init()
+        {
+            Puts("NoBaseDecayWhenOffline init");
+        }
+    }
+}
