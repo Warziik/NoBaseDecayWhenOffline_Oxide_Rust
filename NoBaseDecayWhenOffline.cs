90ea62955dcb8e68659ee24049526344b8df1095
Alex Chesnay <me@alex-chesnay.fr>
2021-10-31 15:24:46 +0100

    feat: disable decay when entity owner is offline

--- NoBaseDecayWhenOffline.cs
+++ NoBaseDecayWhenOffline.cs
@@ -1,16 +1,31 @@
-﻿using System;
-using Oxide.Core.Libraries.Covalence;
-using Oxide.Plugins;
+﻿using Rust;
 
 namespace Oxide.Plugins
 {
     [Info("NoBaseDecayWhenOffline", "Warziik", "0.1.0")]
-    [Description("Disable base decaying when all players allowed on tool cupboard are offline.")]
+    [Description("Disable base decaying when all players allowed on a tool cupboard are offline.")]
     class NoBaseDecayWhenOffline: CovalencePlugin
     {
-        private void init()
+        void init()
         {
             Puts("NoBaseDecayWhenOffline init");
+        }
+
+        object OnEntityTakeDamage(BaseCombatEntity entity, HitInfo info)
+        {
+            if (info == null || info.damageTypes == null || entity == null || !info.damageTypes.Has(DamageType.Decay)) return null;
+
+            BuildingPrivlidge buildingPrivlidge = entity.GetBuildingPrivilege();
+            if (buildingPrivlidge == null || !buildingPrivlidge.AnyAuthed()) return null;
+            
+            BasePlayer player = BasePlayer.Find(entity.OwnerID.ToString());
+            if (player != null)
+            {
+                if (player.IsConnected) return null;
+                else return true;
+            }
+
+            return null;
         }
     }
 }
